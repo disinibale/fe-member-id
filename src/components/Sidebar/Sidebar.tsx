1616664418213379b74e1a38b2902aae75bd49c9
Muhamad Iqbal Nurzaman <iqbal.nurzaman@andalin.com>
2023-08-29 06:01:16 +0700

    Create backdrop for sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -13,28 +13,30 @@ type Props = {
 export default function Sidebar({ isOpen, onClose }: Props) {
 
     return (
-        // <div className={`fixed flex flex-col z-30 top-0 left-0 h-full py-4 px-6 w-64 bg-gray-800 text-white shadow-xl shadow-blue-gray-900/5 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-        <Card className={`fixed flex flex-col z-30 top-0 left-0 h-full py-4 px-6 w-64 shadow-xl shadow-blue-gray-900/5 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-            <div className="flex flex-row justify-between align-left text-lg font-semibold">
-                <Typography variant="h3">
-                    Awards Menu
-                </Typography>
-                <button className="font-bolder" onClick={onClose}>×</button>
-            </div>
-            <div className="flex flex-col justify-start gap-4 mt-8">
-                <Typography variant="lead">
-                    <Link className="" to='/home' onClick={onClose}>Home</Link>
-                </Typography>
-                <Typography variant="lead" className="disabled">
-                    <button className="disabled:text-gray-500 cursor-not-allowed" disabled={true} onClick={onClose}>Cards</button>
-                </Typography>
-                <Typography variant="lead">
-                    <button className="disabled:text-gray-500 cursor-not-allowed" disabled={true} onClick={onClose}>Profile</button>
-                </Typography>
-                <Typography variant="lead" className="disabled">
-                    <Link className="" to='/logout' onClick={onClose}>Logout</Link>
-                </Typography>
-            </div>
-        </Card>
+        <>
+            <div onClick={onClose} className={`${isOpen ? 'absolute' : 'hidden'} left-0 top-0 min-h-full min-w-full bg-black bg-opacity-40 z-[28]`}></div>
+            <Card className={`fixed flex flex-col z-30 top-0 left-0 h-full py-4 px-6 w-64 shadow-xl shadow-blue-gray-900/5 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+                <div className="flex flex-row justify-between align-left text-lg font-semibold">
+                    <Typography variant="h3">
+                        Awards Menu
+                    </Typography>
+                    <button className="font-bolder" onClick={onClose}>×</button>
+                </div>
+                <div className="flex flex-col justify-start gap-4 mt-8">
+                    <Typography variant="lead">
+                        <Link className="" to='/home' onClick={onClose}>Home</Link>
+                    </Typography>
+                    <Typography variant="lead" className="disabled">
+                        <button className="disabled:text-gray-500 cursor-not-allowed" disabled={true} onClick={onClose}>Cards</button>
+                    </Typography>
+                    <Typography variant="lead">
+                        <button className="disabled:text-gray-500 cursor-not-allowed" disabled={true} onClick={onClose}>Profile</button>
+                    </Typography>
+                    <Typography variant="lead" className="disabled">
+                        <Link className="" to='/logout' onClick={onClose}>Logout</Link>
+                    </Typography>
+                </div>
+            </Card>
+        </>
     )
 }
